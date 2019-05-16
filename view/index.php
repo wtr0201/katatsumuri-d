401ab3010ba08eed3c2151d4a4cb9442a0b1c43b
wtr0201 <wataru@sanga.kyoto>
2019-05-16 12:23:47 +0900

    ディレクトリ構造の変更

---
+++ view/index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1 minimum-scale=1,maximum-scale=1,user-scalable=no">
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <title>かたつむり大作戦！ - 交通安全活動</title>
+    <meta name="description" content='ゆっくり歩く"かたつむり"のように、安全第一にサッカー観戦を楽しむ活動「かたつむり大作戦！」です。'>
+    <meta name="keywords" content="かたつむり大作戦,交通安全,サッカー,車遠征">
+    <meta property="og:title" content="私は交通安全活動の「かたつむり大作戦！」に賛同します！">
+    <meta property="og:site_name" content="かたつむり大作戦！ - 交通安全活動">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://katatsumuri-d.com/">
+    <meta property="og:image" content="http://katatsumuri-d.com/img/ogpimg.jpg">
+    <meta property="og:description" content='ゆっくり歩く"かたつむり"のように、安全第一にサッカー観戦を楽しむ活動「かたつむり大作戦！」です。'>
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@sanga_saporen">
+    <meta name="twitter:creator" content="@sanga_saporen">
+    <meta name="twitter:title" content="かたつむり大作戦！ - 交通安全活動">
+    <link rel="stylesheet" href="style.min.css">
+</head>
+
+<body>
+<header>
+    <div id="velttext">
+        <div class="logo"><img class="logoimg" src="./img/logo.svg" alt="かたつむり大作戦ロゴ" ondragstart="return false;" oncontextmenu="return false;"></div>
+        <div class="btn"><a href="#effect">かたつむり大作戦とは？</a></div>
+    </div>
+</header>
+<nav>
+    <div class="navlogo">
+        <a href="#"><img src="./img/logo.svg" alt="かたつむり大作戦ロゴ" ondragstart="return false;" oncontextmenu="return false;"></a>
+    </div>
+    <div class="navicon"><span></span><span></span><span></span></div>
+    <ul id="menu">
+        <li><a href="#effect">趣旨</a></li>
+        <li><a href="#points">3箇条</a></li>
+        <li><a href="#share">共有する</a></li>
+        <li><a href="#news">ニュース</a></li>
+        <li><a href="#approval">賛同団体</a></li>
+        <li><a href="#distribution">配布場所</a></li>
+        <li><a href="#contact">問い合わせ先</a></li>
+    </ul>
+</nav>
+<div id="wrapper">
+    <div id="effect">
+        <h2>趣旨</h2>
+        <hr>
+        <p>我々サポーターは、自チームのホームスタジアムでの観戦、応援のみならず、アウェイスタジアムへも足を運び、その交通手段として公共交通機関以外にも自家用車等で遠征することが多くある中では、交通事故等に巻き込まれる危険も大きいと考えます。</p>
+        <p>新聞やニュースでも大きな交通事故が取り上げられる昨今、悲しい事故で大切な仲間を喪わずにすむように、今一度、交通安全、スタジアムでの安全を心掛け、互いに声を掛け合い意識を高めることが重要だと感じています。</p>
+        <p>そのような中で、我々京都サンガF.C.サポーターは、遠征等での交通安全、スタジアムでの安全を強く意識し、道中無事に過ごせる願いを込めたステッカーを作成、配布することで、交通安全の啓蒙活動をして行きたいと思います。</p>
+        <p>かたつむり大作戦！はゆっくり歩く“かたつむり”のように安全第一にサッカー観戦を楽しむ活動です。</p>
+    </div>
+    <div id="points">
+        <h2>かたつむり大作戦 3箇条</h2>
+        <hr><img src="./img/points.svg" alt="・運転手は無理をしない。・同乗者は無理をさせない。・後部座席もシートベルトを締める。"></div>
+    <p style="text-align:center;"><strong>愛するクラブの勝利を願うように、あなたが無事に帰ることを願う人がいます。<br> サッカーを愛する人すべてが安全に、観戦出来るようにかたつむり大作戦！にご参加ください。</strong></p>
+    <div id="share">
+        <h2>共有する</h2>
+        <hr>
+        <ul>
+            <li>
+                <a href="http://twitter.com/intent/tweet?url=http://katatsumuri-d.com/&amp;text=私は交通安全運動の「%20%23かたつむり大作戦！%20」に賛同します！&amp;tw_p=tweetbutton" data-slimstat-tracking="true" data-slimstat-callback="true" data-slimstat-type="0"><img src="./img/twitter.svg" alt="twitter">twitter</a>
+            </li>
+            <li>
+                <a href="http://www.facebook.com/sharer.php?src=bm&amp;u=http://katatsumuri-d.com/&amp;t=" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;" data-slimstat-tracking="true" data-slimstat-callback="true" data-slimstat-type="0"><img src="./img/facebook.svg" alt="Facebook">Facebook</a>
+            </li>
+            <li>
+                <a href="http://line.me/R/msg/text/?私は交通安全運動の「かたつむり大作戦！」に賛同します！%0Ahttp://katatsumuri-d.com/" data-slimstat-tracking="true" data-slimstat-callback="true" data-slimstat-type="0"><img src="./img/LINE.svg" alt="LINE">LINE</a>
+            </li>
+            <li>
+                <a href="mailto:?body=私は交通安全運動の「かたつむり大作戦！」に賛同します！%0Ahttp://katatsumuri-d.com/" data-slimstat-tracking="true" data-slimstat-callback="true" data-slimstat-type="0"><img src="./img/mail.svg" alt="mail">mail</a>
+            </li>
+        </ul>
+    </div>
+    <div id="news">
+        <h2>ニュースリリース</h2>
+        <hr>
+        <ul class="detail">
+            <?php foreach (array_reverse($news) as $key => $column):?>
+                <li>
+                    <p><a class="accdetail" data-target="detail-<?=$key?>"><?=$column['title']?>(<?=$column['date']?>)</a></p>
+                    <ul class="detail-column" id="detail-<?=$key?>">
+                        <li>
+                            <?php foreach ($column['text'] as $row):?>
+                                <p><?=$row?></p>
+                            <?php endforeach;?>
+                        </li>
+                    </ul>
+                </li>
+            <?php endforeach;?>
+        </ul>
+    </div>
+    <div id="approval">
+        <h2>主な賛同団体<span style="font-size:small;">(敬称略)</span></h2>
+        <hr>
+        <ul>
+            <?php foreach ($approvals as $approval): ?>
+                <li><a href="<?= $approval['url'] ?>" target="_blank"><?= $approval['name'] ?></a></li>
+            <?php endforeach?>
+        </ul>
+    </div>
+    <div id="distribution">
+        <h2>ステッカー配布場所<span style="font-size:small;">(敬称略)</span></h2>
+        <hr>
+        <ul>
+            <?php foreach ($distributions as $distribution): ?>
+                <li><a href="<?= $distribution['url'] ?>" target="_blank"><?= $distribution['name'] ?></a></li>
+            <?php endforeach?>
+        </ul>
+        <p style="font-size:smaller">※配布が終了している場合もございます。予めご了承ください。</p>
+    </div>
+    <div id="contact">
+        <h2>問い合わせ先</h2>
+        <hr>
+        <p>京都サンガF.C.サポーター連合会 <a href="mailto:webmaster@sanga-saporen.net">webmaster@sanga-saporen.net</a></p>
+    </div>
+    <?php
+    //TODO:: ハッシュタグツイート一覧入れたい
+    ?>
+</div>
+<footer><a href="http://www.sanga-saporen.net/" target="_blank">© 2015 Kyoto Sanga F.C. Supporter Confederation </a></footer>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+<script type="text/javascript">
+    $(function () {
+        var headerHight = 60;
+        $('a[href^=#]').click(function () {
+            var href = $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top - headerHight;
+            $("html, body").animate({
+                scrollTop: position
+            }, 950, "swing");
+            return false;
+        });
+    });
+    $(function () {
+        var nav = $('nav');
+        var offsetTop = nav.offset().top;
+
+        var floatMenu = function () {
+            if ($(window).scrollTop() > offsetTop) {
+                nav.addClass('fixed');
+                nav.addClass('fixed');
+            } else {
+                nav.removeClass('fixed');
+            }
+        }
+        $(window).scroll(floatMenu);
+        $('body').bind('touchmove', floatMenu);
+    });
+    window.onload = init;
+
+    function init() {
+        var elements = document.getElementsByClassName("logoimg");
+        for (var i = elements.length; i--;) {
+            disableDraggingFor(elements[i]);
+        }
+    }
+
+    function disableDraggingFor(element) {
+        element.draggable = false;
+        element.onmousedown = function (event) {
+            event.preventDefault();
+            return false;
+        };
+    }
+    $(document).ready(function ($) {
+        $(".navicon").on("click", function () {
+            $("#menu").toggleClass("active");
+            $(this).toggleClass("active");
+        });
+    });
+    $(window).load(function () {
+        function changeWidget() {
+            var twFrame = $('iframe.twitter-timeline');
+            if (twFrame.length > 0) {
+                twFrame.contents()
+                    .find('head').append('<link href="twitter.css" rel="stylesheet" type="text/css" media="all" />');
+            } else {
+                setTimeout(changeWidget, 1500);
+            }
+        }
+        changeWidget();
+    });
+    $(function () {
+        $('.accdetail').click(function () {
+            var target = $(this).data('target');
+
+            $('#' + target).slideToggle();
+
+
+            $(this).toggleClass('open');
+
+            return false;
+        });
+    });
+</script>
+</body>
+
+</html>
